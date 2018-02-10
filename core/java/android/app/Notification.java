5eef09ff467219128af8f327c7f1295d5bafd991
SpiritCroc <spiritcroc@gmail.com>
2018-02-10 09:35:15 +0100

    [Substratum] Themes: add bool to optimize dynamic notification colors on dark backgrounds
    Overlay config_useDarkBgNotificationIconTextTinting to enable dark bg tinting
    (Android 8.1 version)
    
    Changes made on Android Pie:
     * Remove color spans when using config_useDarkBgNotificationIconTextTinting.
       Some notifications use color spans while assuming a dark background,
       leading to unreadable text, so better remove them.
    
    Change-Id: I1c6043abde22c64c2f4ff1287ca209a25e4980c4
    Signed-off-by: Adarsh-MR <adarshmr1998@gmail.com>

--- core/java/android/app/Notification.java
+++ core/java/android/app/Notification.java
@@ -4439,6 +4439,11 @@ public class Notification implements Parcelable
         private CharSequence processTextSpans(CharSequence text) {
             if (hasForegroundColor()) {
                 return NotificationColorUtil.clearColorSpans(text);
+            } else if (mContext.getResources()
+                .getBoolean(R.bool.config_useDarkBgNotificationIconTextTinting)) {
+                // Some notifications have color spans, assuming a dark background,
+                // so let's remove them
+                return NotificationColorUtil.clearColorSpans(text);
             }
             return text;
         }
@@ -5465,8 +5470,10 @@ public class Notification implements Parcelable
                 ensureColors();
                 color = NotificationColorUtil.resolveDefaultColor(mContext, background);
             } else {
+                boolean isDark = mInNightMode || mContext.getResources()
+                        .getBoolean(R.bool.config_useDarkBgNotificationIconTextTinting);
                 color = NotificationColorUtil.resolveContrastColor(mContext, mN.color,
-                        background, mInNightMode);
+                        background, isDark);
             }
             if (Color.alpha(color) < 255) {
                 // alpha doesn't go well for color filters, so let's blend it manually
